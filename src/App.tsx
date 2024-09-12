b9fb54e9fcdc43faeb41ed848c407153cb9e7abd
Lucas Melo <lucasmfs21@gmail.com>
2024-09-12 14:59:46 -0300

    fim da segunda etapa

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import { Dialog } from './components/ui/dialog'
+
+export function App() {
+  return (
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
+    </Dialog>
+  )
+}
+
+export default App
